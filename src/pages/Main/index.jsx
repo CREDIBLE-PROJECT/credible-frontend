23ea84cb393cf0046e2f94ddc12ae51f0aa61c9c
NesquikPlz <ok2006818@gmail.com>
2022-08-13 00:30:56 +0900

    #4 Test Commit

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import { MainLogo } from 'assets';
 import { Menu } from 'components';
-import { StyledRoot, Text } from './style';
+import { StyledRoot, Text, SubText } from './style';
 
 function Main() {
   return (
     <StyledRoot>
       <Menu />
       <Text>Main page</Text>
+      <SubText>hi</SubText>
       <img src={MainLogo} alt="로고" />
     </StyledRoot>
   );

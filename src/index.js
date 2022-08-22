1b324710fa21aba9f9a2f246b668b6441878ce83
Eunbi Choi <pisr25@naver.com>
2022-08-22 13:35:01 +0900

    #1 [Feat] fix color & add fonts

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from 'App';
 import theme from 'styles/theme';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/global-style';
+import GlobalFonts from 'styles/styleOptions/fonts/fonts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
+    <GlobalFonts />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>

43381f6e28d167184b9d17d2f538b2aea17350de
Eunbi Choi <pisr25@naver.com>
2022-09-11 21:55:20 +0900

    #15 [Feat] XAI Report

---
+++ src/components/Common/Result/index.jsx
@@ -0,0 +1,86 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Receipt, Pig } from 'assets';
+import moment from 'moment';
+import {
+  StyledRoot,
+  FirstHorizon,
+  ReportMain,
+  ReportImg,
+  ReportContent,
+  ReportTitle,
+  ReportItem,
+  ReportText,
+  SecondHorizon,
+  ThirdHorizon,
+  DetailText,
+} from './style';
+
+function Result() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledRoot>
+      <ReportMain>
+        <pre>김이화님의 AI 신용점수는 </pre>
+        <pre className="blue">743점</pre>
+        <pre>입니다.</pre>
+      </ReportMain>
+      <ReportImg src={Receipt} alt="영수증 이미지" />
+      <ReportContent>
+        <p className="date">{moment().format('YYYY.MM.DD')}</p>
+        <img src={Pig} alt="돼지저금통 이미지" className="pig" />
+        <ReportTitle>
+          <pre className="sub1">내 신용점수 </pre>
+          <p className="sub2">XAI 레포트</p>
+        </ReportTitle>
+        <ReportItem>
+          <p className="item">항목</p>
+          <p className="percent">상위%</p>
+        </ReportItem>
+        <FirstHorizon width="30%" />
+        <ReportText>
+          <p className="report1">마지막 거래 이후 경과된 개월 수</p>
+          <p className="percent1">35.4%</p>
+        </ReportText>
+        <ReportText>
+          <p className="report2">연체되지 않은 거래의 비율</p>
+          <p className="percent2">65.0%</p>
+        </ReportText>
+        <ReportText>
+          <p className="report3">최근 12개월 중 가장 긴 연체기간</p>
+          <p className="percent3">14.5%</p>
+        </ReportText>
+        <ReportText>
+          <p className="report4">총 거래 횟수</p>
+          <p className="percent4">52.3%</p>
+        </ReportText>
+        <ReportText>
+          <p className="report5">신용 한도 대비 신용카드 잔액</p>
+          <p className="percent5">44.9%</p>
+        </ReportText>
+        <ReportText>
+          <p className="report6">최근 6개월 간 문의 건수</p>
+          <p className="percent6">70.1%</p>
+        </ReportText>
+        <SecondHorizon width="30%" />
+        <ReportItem>
+          <p className="total">Total</p>
+          <p className="totalPercent">47.0%</p>
+        </ReportItem>
+        <ThirdHorizon width="30%" />
+        <DetailText
+          role="presentation"
+          onClick={() => {
+            navigate('/detail');
+          }}
+        >
+          항목 별 더 자세히 확인하기 →
+        </DetailText>
+      </ReportContent>
+    </StyledRoot>
+  );
+}
+
+export default Result;
---
+++ src/components/Common/Result/style.js
@@ -0,0 +1,232 @@
+/* eslint-disable import/no-unresolved */
+import styled from 'styled-components';
+
+const StyledRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  font-family: 'Noto Sans', [monospace];
+`;
+
+const ReportMain = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-family: 'Noto Sans', [monospace];
+  font-size: ${({ theme: { font } }) => font.size.regular};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+  margin: 3rem 0rem 3rem 25rem;
+  position: absolute;
+
+  .blue {
+    color: ${({ theme: { color } }) => color.blue};
+  }
+`;
+
+const ReportImg = styled.img`
+  width: 70rem;
+  height: 83rem;
+  top: 7rem;
+  left: 6.6rem;
+  position: absolute;
+`;
+
+const ReportContent = styled.div`
+  .date {
+    color: ${({ theme: { color } }) => color.grey};
+    position: absolute;
+    left: 20.5rem;
+    top: 10rem;
+  }
+
+  .pig {
+    position: absolute;
+    width: 5rem;
+    height: 5rem;
+    left: 38rem;
+    top: 13rem;
+  }
+`;
+
+const ReportTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  left: 29.5rem;
+  top: 19.8rem;
+  font-size: ${({ theme: { font } }) => font.size.semiMedium};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+
+  .sub1 {
+    color: ${({ theme: { color } }) => color.grey};
+  }
+
+  .sub2 {
+    color: ${({ theme: { color } }) => color.blue};
+  }
+`;
+
+const ReportItem = styled.div`
+  font-weight: ${({ theme: { font } }) => font.weight.light};
+
+  .item {
+    position: absolute;
+    left: 20.5rem;
+    top: 30.2rem;
+  }
+
+  .percent {
+    position: absolute;
+    left: 59.5rem;
+    top: 30.2rem;
+  }
+
+  .total {
+    position: absolute;
+    left: 20.5rem;
+    top: 75.2rem;
+  }
+
+  .totalPercent {
+    position: absolute;
+    left: 59.5rem;
+    top: 75.2rem;
+  }
+`;
+
+// const HorizonLine = styled.hr`
+//   width: ${props => props.width};
+//   border: dashed 0.05rem ${({ theme: { color } }) => color.lighterGrey};
+
+//   position: absolute;
+//   left: 20rem;
+//   top: 32.8rem;
+// `;
+
+const FirstHorizon = styled.hr`
+  width: ${props => props.width};
+  border: dashed 0.05rem ${({ theme: { color } }) => color.lighterGrey};
+
+  position: absolute;
+  left: 20rem;
+  top: 32.8rem;
+`;
+
+const ReportText = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: ${({ theme: { color } }) => color.lighterGrey};
+  font-size: ${({ theme: { font } }) => font.size.small};
+
+  .report1 {
+    position: absolute;
+    left: 20.5rem;
+    top: 40.2rem;
+  }
+
+  .percent1 {
+    position: absolute;
+    left: 58.2rem;
+    top: 40.2rem;
+  }
+
+  .report2 {
+    position: absolute;
+    left: 20.5rem;
+    top: 45.2rem;
+  }
+
+  .percent2 {
+    position: absolute;
+    left: 58.2rem;
+    top: 45.2rem;
+  }
+
+  .report3 {
+    position: absolute;
+    left: 20.5rem;
+    top: 50.2rem;
+  }
+
+  .percent3 {
+    position: absolute;
+    left: 58.2rem;
+    top: 50.2rem;
+  }
+
+  .report4 {
+    position: absolute;
+    left: 20.5rem;
+    top: 55.2rem;
+  }
+
+  .percent4 {
+    position: absolute;
+    left: 58.2rem;
+    top: 55.2rem;
+  }
+
+  .report5 {
+    position: absolute;
+    left: 20.5rem;
+    top: 60.2rem;
+  }
+
+  .percent5 {
+    position: absolute;
+    left: 58.2rem;
+    top: 60.2rem;
+  }
+
+  .report6 {
+    position: absolute;
+    left: 20.5rem;
+    top: 65.2rem;
+  }
+
+  .percent6 {
+    position: absolute;
+    left: 58.2rem;
+    top: 65.2rem;
+  }
+`;
+
+const SecondHorizon = styled.hr`
+  width: ${props => props.width};
+  border: dashed 0.05rem ${({ theme: { color } }) => color.lighterGrey};
+
+  position: absolute;
+  left: 20rem;
+  top: 72.6rem;
+`;
+
+const ThirdHorizon = styled.hr`
+  width: ${props => props.width};
+  border: dashed 0.05rem ${({ theme: { color } }) => color.lighterGrey};
+
+  position: absolute;
+  left: 20rem;
+  top: 77.8rem;
+`;
+
+const DetailText = styled.p`
+  color: ${({ theme: { color } }) => color.blue};
+  font-size: ${({ theme: { font } }) => font.size.regular};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+  position: absolute;
+  left: 38.4rem;
+  top: 80.8rem;
+`;
+
+export {
+  StyledRoot,
+  FirstHorizon,
+  ReportMain,
+  ReportImg,
+  ReportContent,
+  ReportTitle,
+  ReportItem,
+  ReportText,
+  SecondHorizon,
+  ThirdHorizon,
+  DetailText,
+};

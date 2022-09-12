d034b9a904030654b9b868ef2b3baa8d3bfb61de
duswn137 <100579479+duswn137@users.noreply.github.com>
2022-09-12 11:41:28 +0900

    #3 [Style] Join page pc UI

--- src/pages/Join/index.jsx
+++ src/pages/Join/index.jsx
@@ -1,13 +1,103 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-import { MainLogo } from 'assets';
-import { StyledRoot, Text } from './style';
+import { Menu } from 'components';
+import { useNavigate } from 'react-router-dom';
+import { LoginLogo } from 'assets';
+import {
+  StyledRoot,
+  TopNavi,
+  MainBox,
+  Text,
+  JoinForm,
+  BirthBox,
+  SelectBox,
+  JoinButton,
+} from './style';
 
 function Join() {
+  const navigate = useNavigate();
   return (
     <StyledRoot>
-      <Text>Join page</Text>
-      <img src={MainLogo} alt="로고" />
+      <TopNavi>
+        {' '}
+        <p
+          role="presentation"
+          onClick={() => {
+            navigate('/login');
+          }}
+        >
+          로그인
+        </p>
+        <p
+          role="presentation"
+          onClick={() => {
+            navigate('/join');
+          }}
+        >
+          회원가입
+        </p>
+      </TopNavi>
+      <Menu />
+
+      <MainBox>
+        <img src={LoginLogo} alt="로고" />
+
+        <Text>
+          <p> 아래 항목을 입력하세요. </p>
+        </Text>
+
+        <JoinForm>
+          <input type="id" id="id" placeholder="아이디" />
+        </JoinForm>
+        <JoinForm>
+          <input type="password" id="password" placeholder="비밀번호" />
+        </JoinForm>
+        <JoinForm>
+          <input
+            type="passwordcheck"
+            id="password"
+            placeholder="비밀번호 재확인"
+          />
+        </JoinForm>
+
+        <JoinForm>
+          <input type="name" id="name" placeholder="이름" />
+        </JoinForm>
+
+        <SelectBox>
+          <option selected disabled>
+            성별
+          </option>
+          <option>여자</option>
+          <option>남자</option>
+        </SelectBox>
+        <Text>
+          <p> 생년월일 </p>
+        </Text>
+
+        <BirthBox>
+          <input type="year" id="year" placeholder="년(4자)" />
+          <input type="monthselect" id="monthselect" placeholder="월(2자)" />
+          <input type="day" id="day" placeholder="일(2자)" />
+        </BirthBox>
+
+        <JoinForm>
+          <input
+            type="email"
+            id="email"
+            placeholder="이메일을 입력하세요(선택)"
+          />
+        </JoinForm>
+
+        <JoinButton
+          onClick={() => {
+            navigate('/login');
+          }}
+        >
+          {' '}
+          회원가입
+        </JoinButton>
+      </MainBox>
     </StyledRoot>
   );
 }
--- src/pages/Join/style.js
+++ src/pages/Join/style.js
@@ -3,22 +3,125 @@ import styled from 'styled-components';
 import { applyMediaQuery } from 'styles/mediaQuery';
 
 const StyledRoot = styled.div`
-  width: 70vw;
-  height: 30vw;
+  width: 100vw;
+  height: 150vh;
 
-  .img {
-    width: 10rem;
-    height: 3rem;
-  }
+  background-color: ${({ theme: { color } }) => color.lightestBlue};
 
   ${applyMediaQuery('mobile')} {
     display: block;
   }
 `;
+const TopNavi = styled.p`
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  font-family: 'Noto Sans', [monospace];
+  font-size: ${({ theme: { font } }) => font.size.small};
+  color: ${({ theme: { color } }) => color.grey};
 
-const Text = styled.p`
-  font-family: 'Noto Sans';
-  font-size: ${({ theme: { font } }) => font.size.regular};
+  p {
+    margin-right: 2.4rem;
+    margin-top: 1.7rem;
+  }
 `;
 
-export { StyledRoot, Text };
+const MainBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+
+  img {
+    width: 30rem;
+    height: 18rem;
+  }
+`;
+
+const Text = styled.p`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Noto Sans';
+  font-size: ${({ theme: { font } }) => font.size.micro};
+  margin-bottom: 0;
+
+  p {
+    text-align: left;
+  }
+`;
+
+const JoinForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  font-family: 'Noto Sans';
+  padding: 1rem;
+  margin: 0.7rem;
+  input {
+    width: 25rem;
+    outline: none;
+    border: none;
+    padding: 1rem;
+  }
+
+  input::placeholder {
+    font-weight: ${({ theme: { font } }) => font.weight.light};
+  }
+`;
+
+const BirthBox = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-items: center;
+  align-items: center;
+  margin: 0.7rem;
+
+  input {
+    outline: none;
+    border: none;
+    width: 8rem;
+    padding: 1rem;
+    margin: 0.3rem;
+  }
+`;
+
+const SelectBox = styled.select`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  width: 25rem;
+  padding: 1rem;
+  margin: 0.7rem;
+  outline: none;
+  border: none;
+`;
+
+const JoinButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  padding: 1rem;
+
+  width: 25rem;
+  outline: none;
+  border: none;
+  margin: 1.7rem;
+  letter-spacing: 0.1rem;
+  background-color: ${({ theme: { color } }) => color.lighterBlue};
+`;
+
+export {
+  StyledRoot,
+  TopNavi,
+  MainBox,
+  Text,
+  JoinForm,
+  BirthBox,
+  SelectBox,
+  JoinButton,
+};

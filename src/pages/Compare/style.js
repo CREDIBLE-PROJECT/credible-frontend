f32942888195b88fb6b1715875d56773369d718c
duswn137 <100579479+duswn137@users.noreply.github.com>
2022-09-18 15:40:24 +0900

    #27 [Style] Compare page pc UI

--- src/pages/Compare/style.js
+++ src/pages/Compare/style.js
@@ -1,24 +1,220 @@
 /* eslint-disable import/no-unresolved */
 import styled from 'styled-components';
-import { applyMediaQuery } from 'styles/mediaQuery';
 
 const StyledRoot = styled.div`
-  width: 70vw;
-  height: 30vw;
+  width: 100vw;
+  height: 95vw;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme: { color } }) => color.lightestBlue};
+`;
 
-  .img {
-    width: 10rem;
-    height: 3rem;
-  }
+const LoginText = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  font-family: 'Noto Sans', [monospace];
+  font-size: ${({ theme: { font } }) => font.size.small};
+  color: ${({ theme: { color } }) => color.grey};
 
-  ${applyMediaQuery('mobile')} {
-    display: block;
+  p {
+    margin-right: 2.4rem;
+    margin-top: 1.7rem;
   }
 `;
 
-const Text = styled.p`
+const ProfileBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  /* justify-content: space-between; */
+  align-items: center;
+  margin: 3rem 3rem 0rem 3rem;
+`;
+
+const ProfileText = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Noto Sans', [monospace];
+  margin-left: 2rem;
+  margin-right: 2rem;
+
+  .small {
+    font-size: ${({ theme: { font } }) => font.size.semiSmall};
+    font-weight: ${({ theme: { font } }) => font.weight.bold};
+    margin-bottom: 1.3rem;
+  }
+
+  .regular {
+    font-size: ${({ theme: { font } }) => font.size.semiMedium};
+    font-weight: ${({ theme: { font } }) => font.weight.bold};
+  }
+`;
+
+const ProfileImg = styled.img`
+  width: 7rem;
+  height: 7rem;
+`;
+
+const CompareMainBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CompareMainText = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-family: 'Noto Sans', [monospace];
+  font-size: ${({ theme: { font } }) => font.size.semiMedium};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+  position: absolute;
+  left: 15rem;
+  top: 33rem;
+`;
+
+const CompareBox = styled.div`
+  width: 80vw;
+  height: 50vh;
+  background: ${({ theme: { color } }) => color.white};
+  border-radius: 1.5rem;
+  position: absolute;
+  left: 15rem;
+  top: 39rem;
+  padding: 5rem;
+`;
+const CompareMain = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const MyScoreBox = styled.div`
+  width: 40vw;
+  height: 25vh;
+  background: ${({ theme: { color } }) => color.white};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-right: solid 1px;
+`;
+
+const MyScoreText = styled.p`
   font-family: 'Noto Sans';
-  font-size: ${({ theme: { font } }) => font.size.regular};
+  font-size: ${({ theme: { font } }) => font.size.semiMedium};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+  padding-top: 3rem;
+  padding-bottom: 6rem;
 `;
 
-export { StyledRoot, Text };
+const MyScore = styled.p`
+  font-family: 'Noto Sans';
+  font-size: ${({ theme: { font } }) => font.size.large};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+`;
+
+const MeanScoreBox = styled.div`
+  width: 40vw;
+  height: 25vh;
+  background: ${({ theme: { color } }) => color.white};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MeanScoreText = styled.p`
+  font-family: 'Noto Sans';
+  font-size: ${({ theme: { font } }) => font.size.semiMedium};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+  padding-top: 3rem;
+  padding-bottom: 6rem;
+`;
+
+const MeanScore = styled.p`
+  font-family: 'Noto Sans';
+  font-size: ${({ theme: { font } }) => font.size.large};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+`;
+
+const CompareResult = styled.div`
+  font-size: ${({ theme: { font } }) => font.size.regular};
+  display: flex;
+  padding-top: 6rem;
+  align-items: center;
+  font-family: 'Noto Sans';
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+  flex-direction: column;
+`;
+
+const MainMajorCompare = styled.div`
+  flex-direction: row;
+  font-family: 'Noto Sans', [monospace];
+  font-size: ${({ theme: { font } }) => font.size.semiMedium};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+  position: absolute;
+  left: 15rem;
+  top: 88rem;
+`;
+
+const MajorCompareText = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-family: 'Noto Sans', [monospace];
+  font-size: ${({ theme: { font } }) => font.size.semiMedium};
+  font-weight: ${({ theme: { font } }) => font.weight.bold};
+  left: 15rem;
+  top: 95rem;
+  margin-bottom: 3rem;
+`;
+
+const MajorCompareBox = styled.div`
+  width: 80vw;
+  height: 45vh;
+  background: ${({ theme: { color } }) => color.white};
+  border-radius: 1.5rem;
+  display: flex;
+  flex-direction: column;
+
+  .main {
+    color: ${({ theme: { color } }) => color.blue};
+    font-size: ${({ theme: { font } }) => font.size.semiMedium};
+    margin-left: 6.5rem;
+    margin-top: 2rem;
+  }
+
+  li {
+    font-size: ${({ theme: { font } }) => font.size.semiMedium};
+    margin-left: 9rem;
+    margin-top: 2rem;
+  }
+
+  .sub {
+    color: ${({ theme: { color } }) => color.grey};
+    font-size: ${({ theme: { font } }) => font.size.micro};
+    font-weight: ${({ theme: { font } }) => font.weight.light};
+    margin-left: 12rem;
+    margin-top: 2rem;
+  }
+`;
+
+export {
+  StyledRoot,
+  LoginText,
+  ProfileBox,
+  ProfileText,
+  ProfileImg,
+  CompareMainBox,
+  CompareMainText,
+  CompareBox,
+  CompareMain,
+  MyScoreBox,
+  MyScoreText,
+  MyScore,
+  MeanScoreBox,
+  MeanScoreText,
+  MeanScore,
+  CompareResult,
+  MainMajorCompare,
+  MajorCompareText,
+  MajorCompareBox,
+};

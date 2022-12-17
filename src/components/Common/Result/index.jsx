44dcc88552d5c7b793e14672f556af566ee93c63
Eunbi Choi <pisr25@naver.com>
2022-12-17 22:33:45 +0900

    #18 [Feat] add onClick bold

--- src/components/Common/Result/index.jsx
+++ src/components/Common/Result/index.jsx
@@ -18,31 +18,71 @@ import {
 
 function Result() {
   const [viewItem, setViewItem] = useState();
-  let isClicked = false;
+  const [weight1, setWeight1] = useState(false);
+  const [weight2, setWeight2] = useState(false);
+  const [weight3, setWeight3] = useState(false);
+  const [weight4, setWeight4] = useState(false);
+  const [weight5, setWeight5] = useState(false);
+  const [weight6, setWeight6] = useState(false);
 
   const viewItem1 = () => {
+    setWeight2(false);
+    setWeight3(false);
+    setWeight4(false);
+    setWeight5(false);
+    setWeight6(false);
     setViewItem(<Item1 />);
-    isClicked = true;
+    setWeight1(true);
   };
 
   const veiwItem2 = () => {
+    setWeight1(false);
+    setWeight3(false);
+    setWeight4(false);
+    setWeight5(false);
+    setWeight6(false);
     setViewItem(<Item2 />);
+    setWeight2(true);
   };
 
   const veiwItem3 = () => {
+    setWeight1(false);
+    setWeight2(false);
+    setWeight4(false);
+    setWeight5(false);
+    setWeight6(false);
     setViewItem(<Item3 />);
+    setWeight3(true);
   };
 
   const veiwItem4 = () => {
+    setWeight1(false);
+    setWeight2(false);
+    setWeight3(false);
+    setWeight5(false);
+    setWeight6(false);
     setViewItem(<Item4 />);
+    setWeight4(true);
   };
 
   const veiwItem5 = () => {
+    setWeight1(false);
+    setWeight2(false);
+    setWeight3(false);
+    setWeight4(false);
+    setWeight6(false);
     setViewItem(<Item5 />);
+    setWeight5(true);
   };
 
   const veiwItem6 = () => {
+    setWeight1(false);
+    setWeight2(false);
+    setWeight3(false);
+    setWeight4(false);
+    setWeight5(false);
     setViewItem(<Item6 />);
+    setWeight6(true);
   };
 
   return (
@@ -67,9 +107,9 @@ function Result() {
         <FirstHorizon />
         <ReportText>
           <button
+            style={{ fontWeight: weight1 ? 'bold' : 'normal' }}
             type="button"
             className="report1"
-            style={{ fontWeight: isClicked ? 'bold' : 'regular' }}
             onClick={viewItem1}
           >
             대출 중인 금액
@@ -77,31 +117,56 @@ function Result() {
           <p className="percent1">40%</p>
         </ReportText>
         <ReportText>
-          <button type="button" className="report2" onClick={veiwItem2}>
+          <button
+            style={{ fontWeight: weight2 ? 'bold' : 'normal' }}
+            type="button"
+            className="report2"
+            onClick={veiwItem2}
+          >
             대출 신청인 수입
           </button>
           <p className="percent2">52%</p>
         </ReportText>
         <ReportText>
-          <button type="button" className="report3" onClick={veiwItem3}>
+          <button
+            style={{ fontWeight: weight3 ? 'bold' : 'normal' }}
+            type="button"
+            className="report3"
+            onClick={veiwItem3}
+          >
             금융 서비스 대출 이용횟수
           </button>
           <p className="percent3">73%</p>
         </ReportText>
         <ReportText>
-          <button type="button" className="report4" onClick={veiwItem4}>
+          <button
+            style={{ fontWeight: weight4 ? 'bold' : 'normal' }}
+            type="button"
+            className="report4"
+            onClick={veiwItem4}
+          >
             상환기간
           </button>
           <p className="percent4">76%</p>
         </ReportText>
         <ReportText>
-          <button type="button" className="report5" onClick={veiwItem5}>
+          <button
+            style={{ fontWeight: weight5 ? 'bold' : 'normal' }}
+            type="button"
+            className="report5"
+            onClick={veiwItem5}
+          >
             주거지 소유 여부
           </button>
           <p className="percent5">93%</p>
         </ReportText>
         <ReportText>
-          <button type="button" className="report6" onClick={veiwItem6}>
+          <button
+            style={{ fontWeight: weight6 ? 'bold' : 'normal' }}
+            type="button"
+            className="report6"
+            onClick={veiwItem6}
+          >
             근무 기간
           </button>
           <p className="percent6">100%</p>
